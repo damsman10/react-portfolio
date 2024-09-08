4d3efead8e0cc797feb4ba20a1d9a3d65df463ec
Damilola <damilolaoni07@gmail.com>
2024-09-08 06:16:37 +0100

    Header nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import Header from "./sticky/Header"
+import './App.css'
 
 const App = () => {
   return (
-    <div>App</div>
+    <Header />
   )
 }
 
---
+++ src/sticky/Header.jsx
@@ -0,0 +1,28 @@
+
+
+const Header = () => {
+  return (
+    <div className="border border-black flex items-center justify-between">
+        <div className="left">
+            <h1>LOGO</h1>
+        </div>
+
+        <div className="right">
+            <div className="menu">
+                <ul>
+                    <li className="">Home</li>
+                    <li>About</li>
+                    <li>Tech Stack</li>
+                    <li>Projects</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+            <div className="icons">
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
